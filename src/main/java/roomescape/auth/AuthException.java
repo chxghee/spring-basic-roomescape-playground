21a5c6a684c6c4f1813367232b9991768dcd68cc
chxghee <francis001021@gmail.com>
2025-06-25 01:26:41 +0900

    refactor: 미션 요구사항-권한 없는 경우 401응답으로 변경

--- src/main/java/roomescape/auth/AuthException.java
+++ src/main/java/roomescape/auth/AuthException.java
@@ -8,7 +8,7 @@ public enum AuthException implements ExceptionCode {
     ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "access token 만료", "access token 만료되었습니다."),
     INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "access token 형식 오류", "access token이 유효하지 않습니다."),
     UNAUTHENTICATED_REQUEST(HttpStatus.UNAUTHORIZED, "인증되지 않은 요청", "로그인이 필요합니다."),
-    FORBIDDEN_ADMIN_ACCESS(HttpStatus.FORBIDDEN, "관리자 권한 없음", "관리자 권한이 필요합니다.")
+    FORBIDDEN_ADMIN_ACCESS(HttpStatus.UNAUTHORIZED, "관리자 권한 없음", "관리자 권한이 필요합니다.")
     ;
 
     private HttpStatus httpStatus;
