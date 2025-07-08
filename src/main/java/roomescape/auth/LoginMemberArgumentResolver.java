712542a686136edc6e6528a6ed98eb31e602867d
chxghee <francis001021@gmail.com>
2025-07-08 16:54:13 +0900

    refactor: 불필요한 임포트문 삭제 및 인터셉터 수정

--- src/main/java/roomescape/auth/LoginMemberArgumentResolver.java
+++ src/main/java/roomescape/auth/LoginMemberArgumentResolver.java
@@ -8,9 +8,6 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import roomescape.common.CookieUtil;
-import roomescape.exception.ApplicationException;
-import roomescape.member.domain.Member;
-import roomescape.member.domain.MemberRepository;
 
 @Component
 public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
@@ -31,6 +28,7 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
 
+        // 어드민 검증 인터셉터를 거친다면 ArgumentResolver 는 토큰 추출 로직 패스
         LoginMember loginMember = (LoginMember) request.getAttribute("loginMember");
         if (loginMember != null) {
             return loginMember;
